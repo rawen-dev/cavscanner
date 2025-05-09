5abe63c0a8751e292abfc4e1e174621ef2fba75e
miakh <2659269+miakh@users.noreply.github.com>
2025-05-10 01:49:32 +0200

    script for releasing

---
+++ release_scripts/.set_appstore_env.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Replace these values with your actual credentials:
+export APP_STORE_CONNECT_KEY_ID="A4G85T9F5F"
+export APP_STORE_CONNECT_ISSUER_ID="994c45ed-fcec-4fc7-8b75-25a92f3e4124"
+
+echo "✅ App Store Connect environment variables set."
